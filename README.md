136cf2e0338b40cb9d74f2ed3e3fd5577f56c31f
nkowne63rt <neutron63zf@gmail.com>
2022-12-01 16:24:59 +0900

    initial

---
+++ README.md
@@ -0,0 +1,9 @@
+# arxivex
+
+arxivの新着メールのTitleとURLを抜き出してくるだけ
+
+```sh
+pbpaste | arxivex | pbcopy
+```
+
+あとはDeepLに貼り付けるだけ
---
+++ index.ts
@@ -0,0 +1,43 @@
+import { readLines } from "io";
+
+const articles: {
+  url: string;
+  title: string;
+}[] = [];
+
+let temp_article: {
+  url?: string;
+  title?: string;
+} = {};
+let in_title = false;
+for await (const line of readLines(Deno.stdin)) {
+  if (line.startsWith("arXiv:")) {
+    temp_article.url = `https://arxiv.org/abs/${
+      (line.match(/\d{4}\.\d{5}/) ?? [])[0]
+    }`;
+  }
+  if (in_title && !line.startsWith("Authors:")) {
+    temp_article.title += line;
+  }
+  if (line.startsWith("Title:")) {
+    in_title = true;
+    temp_article.title = (line.match(/Title: (.*)$/) ?? [])[1];
+  }
+  if (line.startsWith("Authors:")) {
+    in_title = false;
+    const { url, title } = temp_article;
+    if (!url || !title) {
+      continue;
+    }
+    articles.push({
+      url,
+      title,
+    });
+    temp_article = {};
+  }
+}
+
+articles.forEach(({ url, title }) => {
+  console.log(`URL: ${url}`);
+  console.log(`Title: ${title}`);
+});
